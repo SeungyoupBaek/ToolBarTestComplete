057686a72a827336589576cfcef98f2b35f73745
SeungyoupBaek <bsy8220@naver.com>
2014-01-03 14:48:11 +0900

    base
    base

--- ToolBarTest/ViewController.m
+++ ToolBarTest/ViewController.m
@@ -17,7 +17,29 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // 코드를 이용해서 툴바 생성
+    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 160, 320, 60)];
+    
+    // 문자열 버튼
+    UIBarButtonItem *titleButton = [[UIBarButtonItem alloc] initWithTitle:@"Button" style:UIBarButtonItemStyleDone target:self action:@selector(handleButton:)];
+    
+    // 세그먼티드컨트롤 추가
+    NSArray *segments = [NSArray arrayWithObjects:@"A", @"B", @"C", nil];
+    UISegmentedControl *segment = [[UISegmentedControl alloc]initWithItems:segments];
+    segment.frame = CGRectMake(0, 0, 140, 44);
+    UIBarButtonItem *segmentButton = [[UIBarButtonItem alloc] initWithCustomView:segment];
+    
+    // 공백추가
+    UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    
+    // 리프레쉬버튼추가
+    UIBarButtonItem *refreshButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(handleRefresh:)];
+    
+    NSArray *items = [NSArray arrayWithObjects:titleButton, segmentButton, space, refreshButton, nil];
+    [toolbar setItems:items];
+    
+    [self.view addSubview:toolbar];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +47,22 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(IBAction)handleBarButtonClick :(id)sender{
+    UIBarButtonItem *button = (UIBarButtonItem *)sender;
+    if (1 == button.tag) {
+        NSLog(@"왼쪽버튼");
+    }
+    else
+    {
+        NSLog(@"오른쪽버튼");
+    }
+}
+-(void)handleRefresh:(id)sender{
+    NSLog(@"Refresh!");
+}
+-(void)handleButton:(id)sender{
+    NSLog(@"Button Click!");
+}
+
 
 @end
